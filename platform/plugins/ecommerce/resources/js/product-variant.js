1f2e588c8bc0ac9ef1f2ad1552f92b7f65f2b0f6
Phạm Minh Hiếu <hieupm248@gmail.com>
2024-07-23 13:09:24 +0700

    update code

--- platform/plugins/ecommerce/resources/js/product-variant.js
+++ platform/plugins/ecommerce/resources/js/product-variant.js
@@ -1,8 +1,13 @@
 'use strict'
+
+import { randomId } from '../../../../../resources/js/utils/utils'
+
 const templateOption = function (hasImage = false) {
     const item = document.createElement('div')
     item.className = 'item'
-    item.innerHTML = `${hasImage ? `<div class="image">
+    item.innerHTML = `<div class="image" style="display:${
+        hasImage ? 'flex' : 'none'
+    }">
                             <svg class="theme-arco-icon theme-arco-icon-file_image index__uploadIcon--9CPqD"
                                 width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -14,7 +19,7 @@ const templateOption = function (hasImage = false) {
                                     fill-opacity="1"></path>
                             </svg>
                             <span class="index__uploadHint--ZZSk8">Upload image</span>
-                        </div>`: ''}
+                        </div>
                         <div class="content">
                             <div class="content-info">
                                 <input type="text" placeholder="Add another value" />
@@ -43,12 +48,12 @@ const templateOption = function (hasImage = false) {
                                 </div>
                             </div>
                         </div>`
-    return item;
+    return item
 }
 
-const templateItem = function (){
-    const variantItem = document.createElement("div");
-    variantItem.className = "variant-item";
+const templateItem = function () {
+    const variantItem = document.createElement('div')
+    variantItem.className = 'variant-item'
     variantItem.innerHTML = `<div class="product-variant-items">
             <div class="product-variant-header">
                 <div class="variant-item-top">
@@ -73,10 +78,9 @@ const templateItem = function (){
                         </ul>
                     </div>
                 </div>
-                <svg delete-variant class="theme-arco-icon theme-arco-icon-delete w-16 h-16 mr-16 text-gray-3 fill-current cursor-pointer" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7.08333 6.62728C7.26743 6.62728 7.41667 6.77652 7.41667 6.96061V10.9606C7.41667 11.1447 7.26743 11.2939 7.08333 11.2939H6.58333C6.39924 11.2939 6.25 11.1447 6.25 10.9606V6.96061C6.25 6.77652 6.39924 6.62728 6.58333 6.62728H7.08333zM9.41667 11.2939C9.60076 11.2939 9.75 11.1447 9.75 10.9606V6.96061C9.75 6.77652 9.60076 6.62728 9.41667 6.62728H8.91667C8.73257 6.62728 8.58333 6.77652 8.58333 6.96061V10.9606C8.58333 11.1447 8.73257 11.2939 8.91667 11.2939H9.41667z" fill-opacity="1"></path>
-                    <path d="M5.41667 2.62728C5.41667 1.75283 6.12555 1.04395 7 1.04395H9C9.87445 1.04395 10.5833 1.75283 10.5833 2.62728V3.29395H13.6667C13.8508 3.29395 14 3.44318 14 3.62728V4.29395C14 4.47804 13.8508 4.62728 13.6667 4.62728H12.9776L12.4171 12.3868C12.3935 12.7147 12.373 12.9988 12.3374 13.2326C12.2997 13.4802 12.2383 13.7245 12.1044 13.9573C11.9038 14.3064 11.6024 14.5868 11.2397 14.7619C10.9979 14.8786 10.7498 14.9222 10.5 14.942C10.2643 14.9606 9.97955 14.9606 9.65082 14.9606H6.34901C6.02029 14.9606 5.73548 14.9606 5.49975 14.942C5.25003 14.9222 5.00194 14.8786 4.76007 14.7619C4.39741 14.5868 4.09603 14.3064 3.89535 13.9573C3.76151 13.7245 3.70013 13.4802 3.66242 13.2326C3.62682 12.9988 3.60631 12.7147 3.58264 12.3868L3.02223 4.62728H2.33333C2.14924 4.62728 2 4.47804 2 4.29395V3.62728C2 3.44318 2.14924 3.29395 2.33333 3.29395H5.41667V2.62728ZM4.35904 4.62728L4.91074 12.2663C4.9367 12.6257 4.95386 12.8565 4.98056 13.0318C5.00602 13.1991 5.03321 13.2614 5.05131 13.2929C5.11821 13.4092 5.21867 13.5027 5.33955 13.561C5.37227 13.5768 5.43635 13.5994 5.605 13.6128C5.78179 13.6268 6.01328 13.6273 6.3736 13.6273H9.62619C9.98651 13.6273 10.218 13.6268 10.3948 13.6128C10.5634 13.5994 10.6275 13.5768 10.6602 13.561C10.7811 13.5027 10.8816 13.4092 10.9485 13.2929C10.9666 13.2614 10.9938 13.1991 11.0192 13.0318C11.0459 12.8565 11.0631 12.6257 11.089 12.2663L11.6408 4.62728H4.35904ZM9.41667 2.62728C9.41667 2.39716 9.23012 2.21061 9 2.21061H7C6.76988 2.21061 6.58333 2.39716 6.58333 2.62728V3.29395H9.41667V2.62728Z" fill-opacity="1"></path>
-                </svg>
+                <div class="variant-item-action">
+                    ${deleteItemAction()}
+                </div>
             </div>
             <div>
                 <div class="group-product-variant">
@@ -132,25 +136,52 @@ const templateItem = function (){
         </div>
         <div>
             <button type="button" variant-done>Xong</button>
-        </div>`;
-        return variantItem;
+        </div>`
+    return variantItem
 }
 
-class Variant{
-    constructor(element){
+const deleteItemAction = function () {
+    return `<svg delete-variant class="theme-arco-icon theme-arco-icon-delete w-16 h-16 mr-16 text-gray-3 fill-current cursor-pointer" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.08333 6.62728C7.26743 6.62728 7.41667 6.77652 7.41667 6.96061V10.9606C7.41667 11.1447 7.26743 11.2939 7.08333 11.2939H6.58333C6.39924 11.2939 6.25 11.1447 6.25 10.9606V6.96061C6.25 6.77652 6.39924 6.62728 6.58333 6.62728H7.08333zM9.41667 11.2939C9.60076 11.2939 9.75 11.1447 9.75 10.9606V6.96061C9.75 6.77652 9.60076 6.62728 9.41667 6.62728H8.91667C8.73257 6.62728 8.58333 6.77652 8.58333 6.96061V10.9606C8.58333 11.1447 8.73257 11.2939 8.91667 11.2939H9.41667z" fill-opacity="1"></path>
+                <path d="M5.41667 2.62728C5.41667 1.75283 6.12555 1.04395 7 1.04395H9C9.87445 1.04395 10.5833 1.75283 10.5833 2.62728V3.29395H13.6667C13.8508 3.29395 14 3.44318 14 3.62728V4.29395C14 4.47804 13.8508 4.62728 13.6667 4.62728H12.9776L12.4171 12.3868C12.3935 12.7147 12.373 12.9988 12.3374 13.2326C12.2997 13.4802 12.2383 13.7245 12.1044 13.9573C11.9038 14.3064 11.6024 14.5868 11.2397 14.7619C10.9979 14.8786 10.7498 14.9222 10.5 14.942C10.2643 14.9606 9.97955 14.9606 9.65082 14.9606H6.34901C6.02029 14.9606 5.73548 14.9606 5.49975 14.942C5.25003 14.9222 5.00194 14.8786 4.76007 14.7619C4.39741 14.5868 4.09603 14.3064 3.89535 13.9573C3.76151 13.7245 3.70013 13.4802 3.66242 13.2326C3.62682 12.9988 3.60631 12.7147 3.58264 12.3868L3.02223 4.62728H2.33333C2.14924 4.62728 2 4.47804 2 4.29395V3.62728C2 3.44318 2.14924 3.29395 2.33333 3.29395H5.41667V2.62728ZM4.35904 4.62728L4.91074 12.2663C4.9367 12.6257 4.95386 12.8565 4.98056 13.0318C5.00602 13.1991 5.03321 13.2614 5.05131 13.2929C5.11821 13.4092 5.21867 13.5027 5.33955 13.561C5.37227 13.5768 5.43635 13.5994 5.605 13.6128C5.78179 13.6268 6.01328 13.6273 6.3736 13.6273H9.62619C9.98651 13.6273 10.218 13.6268 10.3948 13.6128C10.5634 13.5994 10.6275 13.5768 10.6602 13.561C10.7811 13.5027 10.8816 13.4092 10.9485 13.2929C10.9666 13.2614 10.9938 13.1991 11.0192 13.0318C11.0459 12.8565 11.0631 12.6257 11.089 12.2663L11.6408 4.62728H4.35904ZM9.41667 2.62728C9.41667 2.39716 9.23012 2.21061 9 2.21061H7C6.76988 2.21061 6.58333 2.39716 6.58333 2.62728V3.29395H9.41667V2.62728Z" fill-opacity="1"></path>
+            </svg>`
+}
+
+const hasImageItemAction = function () {
+    return `<label>
+                <input type="checkbox" has-image="" checked="">
+                Add image
+            </label>`
+}
+
+// Quản lý product-variant
+class Variant {
+    constructor(element) {
+        this.variantId = randomId();
         this.variantOptions = [];
-        this.element = element;
+        this.variantStore = {};
+        this.element = element
         this.productVariantSwitch = this.element.querySelector(
             '[switch-product-variant] input'
         )
-        this.productNoVariant = this.element.querySelector('[product-no-variant]')
-        this.productVariant = this.element.querySelector('[product-has-variant]')
-        this.productVariantOptions = this.element.querySelector('[product-variant-options]');
-        this.variantList = this.element.querySelector('[product-variant-list]');
-        this.addVariant = this.productVariant.querySelector("[add-variant]");
+        this.productNoVariant = this.element.querySelector(
+            '[product-no-variant]'
+        )
+        this.productVariant = this.element.querySelector(
+            '[product-has-variant]'
+        )
+        this.productVariantOptions = this.element.querySelector(
+            '[product-variant-options]'
+        )
+        this.variantList = this.element.querySelector('[product-variant-list]')
+        this.addVariant = this.productVariant.querySelector('[add-variant]')
+        this.variantItemAction = this.element.querySelector(
+            '[variant-item-action]'
+        )
+        this.updateVariant = this.element.querySelector('[update-variant]')
     }
 
-    toggleVariant(){
+    toggleVariant() {
         this.productVariantSwitch.addEventListener('change', (e) => {
             const checked = this.productVariantSwitch.checked
             if (checked) {
@@ -163,107 +194,227 @@ class Variant{
         })
     }
 
-    getItems(){
-        this.variantOptions = [];
-        for(let variantItem of this.variantList.children){
-            const variantInstance = new VariantItem(variantItem);
-            this.variantOptions.push(variantInstance);
+    getItems() {
+        this.lengthItems = this.variantList.children.length
+        let countCurrent = 0
+        for (let variantItem of this.variantList.children) {
+            if (this.lengthItems >= 1 && countCurrent === 0) {
+            } else {
+            }
+            // Kiểm tra xem đã tồn tại variantInstance trong variantOption chưa
+            if(!this.variantOptions.some(({variantItemId}) => variantItemId === variantItem.dataset.id)){
+                const variantInstance = new VariantItem(variantItem, this.variantId)
+                this.addEvent(variantInstance)
+                this.variantOptions.push(variantInstance)
+            }
+            countCurrent++
         }
         this.buildData()
     }
 
-    checkLength(){
-        if(this.variantOptions.length >= 3){
-            return false;
-        }
-        return true;
+    addEvent(variantInstance){
+        document.addEventListener(`change-name-${this.variantId}-${variantInstance.variantItemId}`, e => {
+            this.renameKeys(e.detail.old,e.detail.new)
+        })
     }
 
-    addItem(){
+    checkLength() {
+        if (this.variantOptions.length >= 3) {
+            return false
+        }
+        return true
+    }
+
+    addItem() {
         this.addVariant.onclick = () => {
-            if(!this.checkLength()){
-                return false;
+            if (!this.checkLength()) {
+                return false
             }
-            const itemVariant = templateItem();
-            this.variantList.append(itemVariant);
-            this.getItems();
-            if(!this.checkLength()){
-                this.addVariant.style.display = "none";
+            const itemVariant = templateItem()
+            this.variantList.append(itemVariant)
+            this.getItems()
+            if (!this.checkLength()) {
+                this.addVariant.style.display = 'none'
             }
         }
     }
 
-    createTable(){
-        this.table = document.createElement("table");
-        this.table.className = "table"
-        this.tableHead = document.createElement("thead");
+    createTable() {
+        this.table = document.createElement('table')
+        this.table.className = 'table'
+        this.tableHead = document.createElement('thead')
         this.tableHead.innerHTML = `<tr>
-        ${this.variantOptions.map(variant => {
-            return `<th>${variant.name ?? 'Variantion Name'}</th>`;
-        }).join('')}
+        ${this.variantOptions
+            .map((variant) => {
+                return `<th>${variant.name ?? 'Variantion Name'}</th>`
+            })
+            .join('')}
         <th>Retail price</th>
         <th>Quantity</th>
         <th>Seller SKU</th>
         </tr>`
-        this.body = document.createElement("tbody");
+        this.body = document.createElement('tbody')
         this.body.innerHTML = `<tr>
             <td colspan="100%" class="text-center">No available SKUs</td>
         </tr>`
-        this.table.append(this.tableHead, this.body);
-        this.productVariantOptions.append(this.table);
+        this.table.append(this.tableHead, this.body)
+        this.productVariantOptions.append(this.table)
     }
 
-    buildData(isFirst){
+    buildData() {
+        this.updateVariant.onclick = () => {
+            this.initializeVariants();
+        }
+    }
+
+    // Function to generate all possible variants
+    generateVariants() {
+        const combine = (arr) => {
+            if (arr.length === 0) return [[]]
+            const first = arr[0]
+            const rest = arr.slice(1)
+            const restCombined = combine(rest)
+            const result = []
+
+            first.options.forEach((option) => {
+                restCombined.forEach((combination) => {
+                    result.push([option, ...combination])
+                })
+            })
+
+            return result
+        }
         
+        const variants = combine(this.variantOptions)
+        return variants.map((variant) => {
+            const variantObject = {}
+            this.variantOptions.forEach((attr, index) => {
+                variantObject[attr.name] = variant[index]
+            })
+            return variantObject
+        })
+    }
+
+    initializeVariants() {
+        const variants = this.generateVariants();
+        this.variantStore = {};
+        variants.forEach(variant => {
+            const key = JSON.stringify(variant);
+            this.variantStore[key] = {
+                price: 0,
+                quantity: 0,
+                sku: ''
+            };
+        });
+    }
+
+    renameKeys(oldKey, newKey) {
+        const newObj = {};
+        Object.keys(this.variantStore).forEach(key => {
+            const parsedKey = JSON.parse(key);
+            if (parsedKey.hasOwnProperty(oldKey)) {
+                parsedKey[newKey] = parsedKey[oldKey];
+                delete parsedKey[oldKey];
+            }
+            newObj[JSON.stringify(parsedKey)] = this.variantStore[key];
+        });
+        this.variantStore = newObj;
     }
 }
 
-class VariantItem{
-    constructor(el){
-        this.item = el;
-        this.name = null;
-        this.options = [];
-        this.init();
+class VariantItem {
+    constructor(el, variantId) {
+        this.variantId = variantId;
+        this.variantItemId = randomId();
+        this.item = el
+        this.item.dataset.id = this.variantItemId
+        this.name = null
+        this.options = []
+        this.init()
         // Xác định option name
     }
 
-    init(){
-        this.itemHasImage = this.item.querySelector("[has-image]")?.checked || false;
-        this.itemOptions =  this.item.querySelector("[variant-options] .items");
-        this.initOptions();
+    init() {
+        this.inputHasImage = this.item.querySelector('[has-image]')
+        this.hasImageChecked = this.inputHasImage?.checked || false
+        this.itemOptions = this.item.querySelector('[variant-options] .items');
+        this.inputVariantItem = this.item.querySelector("[variant-item]");
+        this.name = this.inputVariantItem.value;
+        this.initName();
+        this.initOptions()
+        this.configHasImage()
     }
 
-    getName(){
-
-    }
-
-    initOptions(){
-        for(let option of this.itemOptions.children){
-            const optionValue = option.querySelector("input");
-            let timeout = null;    
-            optionValue.oninput = () => {
-                // Kiểm tra nếu sau 1s khi nhập xong mà input có giá trị thì tạo 1 hàng mới
-                clearTimeout(timeout);
-                timeout = setTimeout(() => {
-                    if(optionValue.value != ""){
-                        this.itemOptions.append(templateOption(this.itemHasImage))
-                        this.initOptions();
+    initName(){
+        let timeout = null;
+        this.inputVariantItem.onchange = () => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                document.dispatchEvent(new CustomEvent("change-name-"+this.variantId+"-"+this.variantItemId,{
+                    detail: {
+                        old: this.name,
+                        new: this.inputVariantItem.value
                     }
-                }, 1000);
+                }));
+                this.name = this.inputVariantItem.value;
+            }, 1000);
+        }
+    }
+
+    getVariantItems() {
+        this.itemOptions = this.item.querySelector('[variant-options] .items')
+    }
+
+    configHasImage() {
+        if (!this.inputHasImage) return
+        this.inputHasImage.onchange = () => {
+            this.getVariantItems()
+            this.hasImageChecked = this.inputHasImage.checked
+            for (const itemOption of this.itemOptions.children) {
+                const image = itemOption.querySelector('.image')
+                image.style.display = this.hasImageChecked ? 'flex' : 'none'
             }
         }
+    }
+
+    getName() {}
+
+    initOptions() {
+        let optionLast = null
+        Array.from(this.itemOptions.children).forEach((option, index) => {
+            const optionValue = option.querySelector('input')
+            let timeout = null
+            optionLast = optionValue
+            optionValue.oninput = () => {
+                // Kiểm tra nếu sau 1s khi nhập xong mà input có giá trị thì tạo 1 hàng mới
+                clearTimeout(timeout)
+                timeout = setTimeout(() => {
+                    if (optionValue.value != '' && optionLast === optionValue) {
+                        // Kiểm tra có phải cuối cùng hay không
+                        this.itemOptions.append(
+                            templateOption(this.hasImageChecked)
+                        )
+                        this.initOptions()
+                    }
+                    this.pushOption(index, optionValue.value)
+                }, 1000)
+            }
+        })
+    }
+
+    pushOption(index, value) {
+        this.options[index] = value
     }
 }
 
 const variantEls = document.querySelectorAll('[product-variant]')
 for (const variant of variantEls) {
-    const newVariant = new Variant(variant);
-    newVariant.toggleVariant();
-    newVariant.addItem();
-    newVariant.getItems();
-    newVariant.createTable();
-    newVariant.buildData();
+    const newVariant = new Variant(variant)
+    newVariant.toggleVariant()
+    newVariant.addItem()
+    newVariant.getItems()
+    newVariant.createTable()
+    newVariant.buildData()
     // Đầu tiên variant cần được xác định xem thuộc kiểu gì có thể là 1 giá trị tự đặc hoặc gọi ý là color, size
     // Cần kiểm tra nếu variant 1 chưa có thì tự động lấy cái cuối thành cái đầu tiên
 }
-
